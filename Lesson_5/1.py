c64d40d4591a430a55a52093e31a871a3b9b3c05
peoplecanfly1 <Yura388@yandex.ru>
2020-01-26 17:35:10 +0300

    DZ5

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,45 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+
+QTY_PLANTS = (input('Please insert plants names via space: ')).split(' ')
+QTY = len(QTY_PLANTS)
+BASE = {}
+for i in range(len(QTY_PLANTS)):
+    list1 = input(f'Please input EBIT for each quater via space. Plant {QTY_PLANTS[i]}:').split(' ')
+    list1 = [int(item) for item in list1]
+    BASE[QTY_PLANTS[i]] = list1
+
+AVG_VALUE = sum(map(sum, BASE.values())) / QTY
+LIST_LESS = []
+LIST_BIG = []
+for i in range(QTY):
+    if sum(BASE.get(QTY_PLANTS[i])) < AVG_VALUE:
+        LIST_LESS.append(QTY_PLANTS[i])
+    else:
+        LIST_BIG.append(QTY_PLANTS[i])
+
+print(f'Меньше среднего: {LIST_LESS}')
+print(f'Больше среднего: {LIST_BIG}')
+
+# Решение через namedtuple
+
+from collections import namedtuple
+
+AVG_EBIT = 0
+
+COMPANY_QTY = 4
+Company_info = namedtuple('company', 'name ebit')
+company_list = []
+for i in range(COMPANY_QTY):
+    name_c = input('company name: ')
+    total_year = [int(i) for i in input(f'please put an ebit for each quter via space for company {name_c}').split(' ')]
+    company = Company_info(name=name_c, ebit=sum(total_year))
+    company_list.append(company)
+
+for i in company_list:
+    AVG_EBIT += i.ebit
+AVG_EBIT = AVG_EBIT / COMPANY_QTY
+print(f'Avarage EBIT of all companies: {AVG_EBIT}')
+print(f'Companies above avarage EBIT {[i for i in company_list if i.ebit > AVG_EBIT]}')
+print(f'Companies less avarage EBIT {[i for i in company_list if i.ebit < AVG_EBIT]}')
--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,29 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+
+
+class HexOperation:
+    """Перегрузка стандартных сложения и умножения"""
+
+    def __init__(self, first_num, second_num):
+        self.first_num = first_num
+        self.second_num = second_num
+
+    def __add__(self, other):
+        return list(hex(int(''.join(self.first_num), 16) +
+                        int(''.join(other.second_num), 16)))[2:]
+
+    def __mul__(self, other):
+        return list(hex(int(''.join(self.first_num), 16) *
+                        int(''.join(other.second_num), 16)))[2:]
+
+
+FIRST_HEX_NUM = list(input('Введите первое шестнадцатиричное число: '))
+SECOND_HEX_NUM = list(input('Введите второе шестнадцатиричное число: '))
+
+RESULT_SUM = HexOperation(FIRST_HEX_NUM, SECOND_HEX_NUM) + \
+    HexOperation(FIRST_HEX_NUM, SECOND_HEX_NUM)
+RESULT_MUL = HexOperation(FIRST_HEX_NUM, SECOND_HEX_NUM) * \
+    HexOperation(FIRST_HEX_NUM, SECOND_HEX_NUM)
+print(f'Сумма = {RESULT_SUM}, а произведение = {RESULT_MUL}')
